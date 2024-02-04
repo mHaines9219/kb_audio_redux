79a9c6fcf9c09872cbd191ea6ce11275d0367e97
mHaines331 <mhaines9219@gmail.com>
2024-02-03 17:30:02 -0800

    navbar setup

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Navbar.css';
-
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 export default function Navbar() {
-  return <div className="navbar">NavBar</div>;
+  return (
+    <>
+      <div className="navbar">
+        <a href="#">PROJECT TEMPLATES</a>
+        <a href="#">VOCAL PRESETS</a>
+        <a href="#">LOGIN</a>
+        <a href="#">SIGN UP</a>
+        <ShoppingCartOutlinedIcon />
+      </div>
+    </>
+  );
 }
