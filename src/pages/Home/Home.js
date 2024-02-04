1fbb726c155a8c1d2085ea411964cc27c375a5bc
mHaines331 <mhaines9219@gmail.com>
2024-02-03 18:20:04 -0800

    install router

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import Navbar from '../components/Navbar/Navbar';
+import Navbar from '../../components/Navbar/Navbar';
+import './Home.css';
 
 export default function Home() {
   return (
     <>
       <Navbar />
+      <h1>Home</h1>
     </>
   );
 }
