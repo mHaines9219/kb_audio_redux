4a193c5384c6bac7dabae858133e250b6035df4e
mHaines331 <mhaines9219@gmail.com>
2024-02-03 17:54:03 -0800

    add logo placeholder to navbar, separate navbar elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,11 +5,24 @@ export default function Navbar() {
   return (
     <>
       <div className="navbar">
-        <a href="#">PROJECT TEMPLATES</a>
-        <a href="#">VOCAL PRESETS</a>
-        <a href="#">LOGIN</a>
-        <a href="#">SIGN UP</a>
-        <ShoppingCartOutlinedIcon />
+        <div className="logo">LOGO</div>
+        <div className="navbar-links-container">
+          <a className="navbar-links" href="#">
+            PROJECT TEMPLATES
+          </a>
+          <a className="navbar-links" href="#">
+            VOCAL PRESETS
+          </a>
+          <a className="navbar-links" href="#">
+            OMNISPHERE PRESETS
+          </a>
+        </div>
+        <div className="navbar-right">
+          <a className="navbar-links" href="#">
+            LOGIN/SIGNUP
+          </a>
+          <ShoppingCartOutlinedIcon />
+        </div>
       </div>
     </>
   );
